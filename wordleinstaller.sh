b01b83b213a1253ed1a49cce09c247a8f8bbef17
daus-s <dauscarmichael@gmail.com>
2023-06-09 14:06:27 -0700

    added writing with approriate path to the wordle executable

--- wordleinstaller.sh
+++ wordleinstaller.sh
@@ -3,16 +3,17 @@ echo "@ wordle macOS installer @"
 echo @@@@@@@@@@@@@@@@@@@@@@@@@@
 
 currentDirectory=$(pwd)
-chmod +x wordle
-
 #in bash
 
 # Check if wordle command already exists in PATH
+path="export PATH=\"\$PATH:${currentDirectory}\"\n"
+
+
+
 if grep -q "export PATH=\"\$PATH:${currentDirectory}\"" ~/.bash_profile; then
     echo "The wordle command is already set up in PATH."
 else
     # Add wordle command to PATH
-    path="export PATH=\"\$PATH:${currentDirectory}\"\n"
     echo "" >> ~/.bash_profile
     echo "# wordle bot command path" >> ~/.bash_profile
     echo -e $path >> ~/.bash_profile
@@ -20,6 +21,21 @@ else
 fi
 
 source ~/.bash_profile
+
+# set up the wordle executable
+echo "cd ${path}\n
+      echo WORDLE\n
+      java -jar Wordle.jar\nSTATUS=\$?\n
+      if [ \$STATUS -ne 0 ]; then\n
+      echo \"error occured, compiling and rebuilding Wordle.jar\"\n
+      javac Wordle.javajar cfe Wordle.jar Wordle Wordle.class\n
+      java -jar Wordle.jar\n
+      fi" >| wordle
+
+
+chmod +x wordle
+
+
 
 echo "Installation complete."
 read -p "Press Enter to exit..."
