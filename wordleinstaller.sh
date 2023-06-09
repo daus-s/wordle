bc08ede2ab251cd128b9226e5fad03021b34be61
daus-s <dauscarmichael@gmail.com>
2023-06-09 13:00:40 -0700

    more robust and reload source

--- wordleinstaller.sh
+++ wordleinstaller.sh
@@ -7,8 +7,19 @@ chmod +x wordle
 
 #in bash
 
-path="export PATH=\"\$PATH:${currentDirectory}/wordle"\"
-echo "" >> ~/.bash_profile
-echo "#wordle bot command path" >> ~/.bash_profile
-echo $path >> ~/.bash_profile 
+# Check if wordle command already exists in PATH
+if grep -q "export PATH=\"\$PATH:${currentDirectory}\"" ~/.bash_profile; then
+    echo "The wordle command is already set up in PATH."
+else
+    # Add wordle command to PATH
+    path="export PATH=\"\$PATH:${currentDirectory}\"\n"
+    echo "" >> ~/.bash_profile
+    echo "# wordle bot command path" >> ~/.bash_profile
+    echo -e $path >> ~/.bash_profile
+    echo "The wordle command has been added to PATH."
+fi
 
+source ~/.bash_profile
+
+echo "Installation complete."
+read -p "Press Enter to exit..."
