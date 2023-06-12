d6542052d0f6147c4a2b485ad96827ae633c0062
daus-s <dauscarmichael@gmail.com>
2023-06-12 16:05:13 -0700

    zsh installer v0.1

--- wordleinstaller.sh
+++ wordleinstaller.sh
@@ -1,33 +1,50 @@
-echo @@@@@@@@@@@@@@@@@@@@@@@@@@
-echo "@ wordle macOS installer @"
-echo @@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo "@ wordlebot macOS installer @"
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 currentDirectory=$(pwd)
 #in bash
 
-# Check if wordle command already exists in PATH
-path="export PATH=\"\$PATH:${currentDirectory}\"\n"
+shell=$SHELL
+echo $shell
+
+if [ "$SHELL" = "/bin/bash" ]; then
+  newpath="export PATH=\"\$PATH:${currentDirectory}\"\n"
+
+    # Check if wordle command already exists in PATH
+    if grep -q "export PATH=\"\$PATH:${currentDirectory}\"" ~/.bash_profile; then
+          echo "The wordle command is already set up in PATH."
+      else
+        # Add wordle command to PATH
+          echo "" >> ~/.bash_profile
+          echo "# wordle bot command path" >> ~/.bash_profile
+          echo -e $newpath >> ~/.bash_profile
+          echo "The wordle command has been added to PATH."
+    fi
+
+    source ~/.bash_profile
 
 
+elif [ "$SHELL" = "/bin/zsh" ]; then
+      newpath="${currentDirectory}"
+      if grep -q "${currentDirectory}\"" ~/.zshrc; then
+            echo "The wordle command is already set up in PATH."
+        else
+          # Add wordle command to PATH
+          echo "export PATH=$PATH:${currentDirectory*}" >> ~/.zshrc
+      fi
 
-if grep -q "export PATH=\"\$PATH:${currentDirectory}\"" ~/.bash_profile; then
-    echo "The wordle command is already set up in PATH."
-else
-    # Add wordle command to PATH
-    echo "" >> ~/.bash_profile
-    echo "# wordle bot command path" >> ~/.bash_profile
-    echo -e $path >> ~/.bash_profile
-    echo "The wordle command has been added to PATH."
-fi
+   else
+       echo "Unknown shell."
+   fi
 
-source ~/.bash_profile
 
 # set up the wordle executable
 echo "cd ${currentDirectory}
       echo WORDLE
       java -jar Wordle.jar
       STATUS=\$?
-      if [ \$STATUS -ne 0 ]; then
+      if [ \$STATUS -eq 1 ]; then
         echo \"error occured, compiling and rebuilding Wordle.jar\"
         javac Wordle.java
         jar cfe Wordle.jar Wordle Wordle.class
